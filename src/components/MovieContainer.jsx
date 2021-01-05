fb1892d632d320f7d32095d7245cff1c9c0a92d7
Przemek Surmicki <surmicki1@gmail.com>
2021-01-05 13:08:35 +0100

    fixes fix

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -9,7 +9,7 @@ const MovieContainer = () => {
   const { data, isLoading } = useData(path);
 
   useEffect(() => {
-    setPath('http://swapi.dev/api/films/');
+    setPath('https://swapi.dev/api/films/');
   }, [path])
 
   return (
